a7e651543d4be107dddfb059c25e93e22163f40d
nampham148 <phamquangnam148@gmail.com>
2018-04-09 18:30:30 -0400

    implement sessions

--- helpers.py
+++ helpers.py
@@ -1,2 +1,18 @@
 def sanitize(string):
 	return string.replace(" ", "%20")
+
+def RepresentsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+def CorrectRequest(s):
+	try:
+		data = s.split(",")
+		s1 = data[0]
+		s2 = data[1]
+		return True
+	except:
+		return False
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from __future__ import unicode_literals
-from flask import Flask, request, redirect, url_for, send_from_directory
+from flask import Flask, request, redirect, url_for, send_from_directory, session
 from twilio.twiml.voice_response import VoiceResponse
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 from pytube import YouTube
-from helpers import sanitize
+from helpers import sanitize, RepresentsInt, CorrectRequest
 import urllib, json
 import os
 import ffmpy
 
+SECRET_KEY = os.environ['SECRET_KEY']
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 account_sid = os.environ['account_sid']
 auth_token = os.environ['auth_token']
@@ -17,6 +19,8 @@ phone_number = os.environ['phone_number']
 home_url = os.environ['home_url']
 API_KEY = os.environ['API_KEY']
 avail_songs=[]
+
+
 
 @app.route("/answer", methods=['GET', 'POST'])
 def answer_call():
@@ -29,49 +33,100 @@ def answer_call():
 
     return str(resp)
 
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	print(session.get('request_number'))
+	body = request.values.get('Body', None)
+	request_number=session.get('request_number', 0)
+		
+	if CorrectRequest(body):
+		data = body.split(",")
+		data[1] = data[1][1:]
+		session['request_number'] = data[1]
+		print[session.get('request_number')]
+		return redirect(url_for('search', song_req=data[0]))
+	elif RepresentsInt(body):
+		sender = request.values.get('From', None)
+		return redirect(url_for('answer_sms', choice=body, sender=sender))
+	else:
+		session.clear()
+		resp = MessagingResponse()
+		resp.message("An error has occurred")
+		return str(resp)
+	
+@app.route("/search", methods=['POST'])
+def search():
+	print("search reached")
+	song_req = request.args['song_req']
+	#search for the songs
+	q = sanitize(song_req) #remove all spaces
+ 	url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q={}&key={}".format(q, API_KEY)
+	response = urllib.urlopen(url)
+	data = json.loads(response.read())
+
+	#start storing lists
+	session['songIds'] = []
+	session['songNames'] = []
+
+	#construct the response
+	resp = MessagingResponse()
+	resp.message("We found three songs for you!")
+
+	#add to storing lists
+	for i in range(3):
+		title = data['items'][i]["snippet"]['title']
+		author = data['items'][i]["snippet"]['channelTitle']
+		print(title, author)
+		resp.message("{}. Song {} by {}".format(i, title, author))
+
+		videoId = data['items'][0]['id']['videoId']
+		session['songIds'].append(videoId)
+		session['songNames'].append(title)
+
+	session['state'] = 1
+	return str(resp)
+
 @app.route("/sms", methods=['GET', 'POST'])
 def answer_sms():
-	body = request.values.get('Body', None)
-	data = body.split(",")
-	data[1] = data[1][1:]
+	#body = request.values.get('Body', None)
+	#data = body.split(",")
+	#data[1] = data[1][1:]
 	
-	request_number = data[1]
-	song_req = data[0]
+	#request_number = data[1]
+	#song_req = data[0]
+	choice=request.args['choice']
+	choice=int(choice)
+	sender=request.args['sender']
+	song_req=session['songNames'][choice]
+	request_number=session['request_number']
+	chosen_id=session['songIds'][choice]
 
-	reply = "Thanks for your message! The request to {} with the song {}".format(request_number, song_req)
+	reply = "Thanks for your message! The request to {} with the song {}".format(session['request_number'], song_req)
 
-	caller = request.values.get('From', None)
 	client = Client(account_sid, auth_token)
 
 	#start the reply message
 	client.api.account.messages.create(
-	    to=caller,
+	    to=sender,
 	    from_=phone_number,
 	    body=reply)
 
-	return redirect(url_for('send_music', request_number=request_number, song_req=song_req), code=302)
+	return redirect(url_for('send_music', request_number=request_number, chosen_id=chosen_id), code=302)
 	
 
 @app.route("/send_music", methods=['POST'])
 def send_music():
- 	request_number=request.args['request_number']
- 	song_req = request.args['song_req']
-
- 	#search for music
- 	q = sanitize(song_req) #remove all spaces
- 	url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q={}&key={}".format(q, API_KEY)
-	response = urllib.urlopen(url)
-	data = json.loads(response.read())
-
+ 	
 	#extract data from API call
-	videoId = data['items'][0]['id']['videoId']
-	file_name = videoId + ".mp3"
+	videoId = choice=request.args['chosen_id']
+	file_name = "{}.mp3".format(videoId)
+	request_number=session['request_number']
 
 	global avail_songs
 	#check if song already available
 	if videoId not in avail_songs:
 		avail_songs.append(videoId)
-		ori_file_name = videoId + ".mp4"
+		ori_file_name = "{}.mp4".format(videoId)
 		
 
 		#download the music
@@ -100,6 +155,8 @@ def send_music():
 	    from_=phone_number,
 	    url="{}/response/{}".format(home_url, file_name)
 	)
+
+	session.clear()
 	return "success!"
 
 @app.route('/Music/<path:path>', methods=['GET', 'POST'])
